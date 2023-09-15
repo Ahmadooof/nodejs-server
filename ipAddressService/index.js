7996fb59a872271e3dd8f2788cb0b8dfc765c1ad
ahmad anbarje <ahmad.a.93@hotmail.com>
2023-09-15 10:11:54 +0300

    test

--- ipAddressService/index.js
+++ ipAddressService/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import morgan from 'morgan';
-// import getClientIP from './middlewear/getClientIP.js';
+import getClientIP from './middlewear/getClientIP.js';
 import usageRoute from './routes/usage.js';
 import userRoute from './routes/user.js';
 import messagesRoute from './routes/messages.js';
-import cors from 'cors';
 
 const allowedOrigin = 'https://ahmadhome.com'; // http://localhost:3000 reactjs
 
@@ -19,23 +19,23 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    console.log('Request Headers:', req.headers);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('Request Headers:', req.headers);
+//     next();
+// });
   
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms')); // for logging development purposes
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms')); // for logging development purposes
 
 app.use(cors(corsOptions));
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello');
 })
 
 app.use('/', userRoute)
 app.use('/', usageRoute)
 app.use('/', messagesRoute);
-// app.use(getClientIP)
+app.use(getClientIP)
 
 app.listen(4000, () => {
 
