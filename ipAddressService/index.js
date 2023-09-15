55102fe2b9b636420ad0d1e8b4c005a7cf2db17f
ahmad anbarje <ahmad.a.93@hotmail.com>
2023-09-15 10:14:49 +0300

    test

--- ipAddressService/index.js
+++ ipAddressService/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 //     next();
 // });
   
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms')); // for logging development purposes
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms')); // for logging development purposes
 
 app.use(cors(corsOptions));
 
