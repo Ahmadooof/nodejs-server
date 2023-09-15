646dfc9bb2ab9a9f9be1773888f5bdfe42b1e9e2
ahmad anbarje <ahmad.a.93@hotmail.com>
2023-09-15 11:10:22 +0300

    some changes

--- ipAddressService/index.js
+++ ipAddressService/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-// import getClientIP from './middlewear/getClientIP.js';
+import getClientIP from './middlewear/getClientIP.js';
 import usageRoute from './routes/usage.js';
 import userRoute from './routes/user.js';
 import messagesRoute from './routes/messages.js';
@@ -19,14 +19,15 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.use(cors(corsOptions));
+
 app.use((req, res, next) => {
     console.log('Request Headers:', req.headers);
     next();
 });
   
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms')); // for logging development purposes
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms')); // for logging development purposes
 
-app.use(cors(corsOptions));
 
 app.get('/', function (req, res) {
   res.send('hello world');
@@ -35,7 +36,7 @@ app.get('/', function (req, res) {
 app.use('/', userRoute)
 app.use('/', usageRoute)
 app.use('/', messagesRoute);
-// app.use(getClientIP)
+app.use(getClientIP)
 
 app.listen(4000, () => {
 
